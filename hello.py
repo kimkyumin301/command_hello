39cafd4b2bddb61bb4f1e7b333e514d2f50e7c85
kyumin <7528kim1@naver.com>
2020-04-27 15:57:53 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-print("hello worldi
+// For command line git tutorial
+// Local repository modification
+
+print("hello world")
 print("Tell your world")
+print("Tell his world")
+print(Tell her world")
